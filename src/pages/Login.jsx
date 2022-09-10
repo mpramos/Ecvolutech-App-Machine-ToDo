7ab2c037ea5647645392ca358a2b48bfadbee19f
PaolaRamosGutierrez <paolagutierrezramos5@gmail.com>
2022-09-10 12:56:59 -0400

    Formularios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import SubmitButton from "../components/buttons/SubmitButton";
 import Container from "../components/container/Container";
 import Form from "../components/forms/Form";
@@ -6,13 +6,20 @@ import Input from "../components/forms/Input";
 import Title from "../components/headers/Title";
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+  const login = (event) => {
+    event.preventDefault();
+    console.log(email.current.value);
+    console.log(password.current.value);
+  };
   return (
     <>
       <Container>
-      <Title>Login</Title>
-        <Form>
-          <Input placeholder="Email" type="email"></Input>
-          <Input placeholder="Password" type="password"></Input>
+        <Title>Login</Title>
+        <Form onSubmit={login}>
+          <Input ref={email} placeholder="Email" type="email"></Input>
+          <Input ref={password} placeholder="Password" type="password"></Input>
           <SubmitButton>Login</SubmitButton>
         </Form>
       </Container>

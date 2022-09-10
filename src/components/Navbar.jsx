1bbad0ffe94a5aeb87b2af7eac8b6f9713a9060d
PaolaRamosGutierrez <paolagutierrezramos5@gmail.com>
2022-09-10 10:56:06 -0400

    Estructura del proyecto y rutas

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <>
+        <Link to="/login">Login</Link>
+        <li>pepitp</li>
+        <li>pepitp</li>
+        <li>pepitp</li>
+        <li>pepitp</li>
+        <li>pepitp</li>
+        <li>pepitp</li>
+        <li>pepitp</li>
+    </>
+  )
+}
